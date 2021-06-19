0a1edc475d2f71106cd1333df78202c72cc5e881
PushpakB3096 <rtpushpak@gmail.com>
2021-06-19 17:04:07 +0530

    Add mongodb atlas for persistence of docs

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const io = require("socket.io")(3001, {
   cors: {
     origin: "http://localhost:3000",
@@ -5,10 +10,32 @@ const io = require("socket.io")(3001, {
   }
 });
 
+// connecting to mongoDB atlas via mongoose
+mongoose
+  .connect(process.env.CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err =>
+    console.error("Some error occurred while connecting to MongoDB Atlas")
+  );
+
 io.on("connection", socket => {
-  // if the socket has some changes on the editor sent by the client
-  socket.on("send-change", delta => {
-    // Broadcast means send the changes to all the users except us
-    socket.broadcast.emit("receive-changes", delta);
+  socket.on("get-document", documentId => {
+    // sets the data to be sent back to the client
+    const data = "";
+    /*
+      On first time load, we need to put the socket in its own room having the ID of documentId.
+      Any other socket can join the same room if they have the room ID.
+    */
+    socket.join(documentId);
+    socket.emit("load-document", data);
+
+    // if the socket has some changes on the editor sent by the client
+    socket.on("send-change", delta => {
+      // Broadcast means send the changes to all the users in the room ID
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
   });
 });
