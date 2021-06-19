29f302000b3fdb1ecd5d3e2b8b5689c250eedede
PushpakB3096 <rtpushpak@gmail.com>
2021-06-19 16:23:22 +0530

    Add router for document IDs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import { v4 as uuid } from "uuid";
 import TextEditor from "./components/TextEditor.jsx";
+
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          {/* when user comes to the homepage, redirect them to a new document with a random ID */}
+          <Redirect to={`/documents/${uuid()}`} />
+        </Route>
+        <Route path='/documents' exact>
+          <Redirect to={`/documents/${uuid()}`} />
+        </Route>
+        <Route path='/documents/:id'>
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
