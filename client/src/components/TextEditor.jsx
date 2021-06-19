fc3568439727d79e981cdf637542f05061ccd412
PushpakB3096 <rtpushpak@gmail.com>
2021-06-19 14:47:58 +0530

    Add render quill editor on mount

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useRef, useCallback } from "react";
 import Quill from "quill";
+
 import "quill/dist/quill.snow.css";
 
 const TextEditor = () => {
-  return <div id='container'>Text Editor</div>;
+  const wrapper = useRef();
+
+  useEffect(() => {
+    // creates an editor containersdsa for placing the quil editor inside including the toolbar
+    const editor = document.createElement("div");
+    wrapper.current.append(editor);
+
+    new Quill(editor, { theme: "snow" });
+
+    // before unmount, remove the quill editor
+    return () => {
+      wrapper.current.innerHTML = "";
+    };
+  }, []);
+
+  return <div id='container' ref={wrapper}></div>;
 };
 
 export default TextEditor;
