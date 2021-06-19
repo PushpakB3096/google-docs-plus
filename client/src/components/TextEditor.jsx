ac332728651096e4f40580b2f805363dd044eab7
PushpakB3096 <rtpushpak@gmail.com>
2021-06-19 15:26:24 +0530

    Add more toolbar options

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -3,6 +3,22 @@ import Quill from "quill";
 
 import "quill/dist/quill.snow.css";
 
+var toolbarOptions = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ direction: "rtl" }], // text direction
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["image", "blockquote", "code-block"],
+  ["clean"] // remove formatting button
+];
+
 const TextEditor = () => {
   const wrapper = useRef();
 
@@ -11,7 +27,7 @@ const TextEditor = () => {
     const editor = document.createElement("div");
     wrapper.current.append(editor);
 
-    new Quill(editor, { theme: "snow" });
+    new Quill(editor, { theme: "snow", modules: { toolbar: toolbarOptions } });
 
     // before unmount, remove the quill editor
     return () => {
